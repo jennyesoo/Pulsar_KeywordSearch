14f30f7316ee123d5abece4d57896fa553ba71b7
jennyesoo <49374999+jennyesoo@users.noreply.github.com>
2023-05-31 14:29:19 +0800

    pro-processing in search (#15)
    * 104226 - Write code : add an input query pre-processing function in Search
    104243 - pro-processing in search : special data types
    104230 - Write code : search setting in Searchquery
    
    * merge to dev

--- HP.Pulsar.Search.Keyword/SearchEngine/SearchClient.cs
+++ HP.Pulsar.Search.Keyword/SearchEngine/SearchClient.cs
@@ -1,13 +1,28 @@
-﻿using HP.Pulsar.Search.Keyword.CommonDataStructure;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using HP.Pulsar.Search.Keyword.CommonDataStructure;
 using HP.Pulsar.Search.Keyword.DataTransformation;
 using HP.Pulsar.Search.Keyword.Infrastructure;
+using Humanizer;
 using Meilisearch;
+using Microsoft.Extensions.FileSystemGlobbing.Internal;
 
 namespace HP.Pulsar.Search.Keyword.SearchEngine;
 
 public class SearchClient
 {
     private readonly MeiliSearchClient _client;
+
+    private static readonly List<Regex> _pattern = new()
+        {
+            //This pattern accepts special character such as "17WWQ1AL6AC", "17wwyzw6#d", "17WWQ1AD3##", "3C17"
+            new Regex(@".*(([A-Za-z]+[0-9]+)+|([0-9]+[A-Za-z]+)+).*", RegexOptions.IgnoreCase),
+            //This pattern accepts special character such as "ext.15973", "1.0", "1.6.0.17", "1.2.281.8344"
+            new Regex(@"[A-Za-z0-9]+[\.]+[0-9]+", RegexOptions.IgnoreCase),
+            //This pattern accepts special character such as "+886-3-327-2345"
+            new Regex(@"[\+]*[0-9]+[\-]+[0-9]+[\-]+[0-9]+[\-]+[0-9]+", RegexOptions.IgnoreCase)
+        };
 
     public SearchClient(KeywordSearchInfo info)
     {
@@ -20,7 +35,12 @@ public class SearchClient
         List<string> handledInput = PreProcess(input);
 
         // search 
-        SearchQuery query = new();
+        SearchQuery query = new SearchQuery
+        {
+            MatchingStrategy = "all",
+            Limit = 700
+        };
+
         IReadOnlyDictionary<SearchType, List<SingleOutputModel>> models = await _client.SearchAsync(string.Join(' ', handledInput), query);
 
         // TODO - post-process 
@@ -34,20 +54,46 @@ public class SearchClient
         string[] inputs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
         List<string> handledInput = new();
-
-        // datetime 
         foreach (string temp in inputs)
         {
-            if (CommonDataTransformer.TryParseDate(temp, out DateTime date))
+            //This pattern accepts special character such as "2-2-0"
+            if (Regex.IsMatch(temp, @"[0-9]+\-[0-9]+"))
+            {
+                handledInput.Add($"\"{temp}\"");
+            }
+            else if (CommonDataTransformer.TryParseDate(temp, out DateTime date))
             {
                 handledInput.Add($"\"{date:yyyy/MM/dd}\"");
             }
+            else if (int.TryParse(temp, out _))
+            {
+                handledInput.Add($"\"{temp}\"");
+            }
+            else if (GetRexAnswer(temp, out string result))
+            {
+                handledInput.Add(result);
+            }
             else
             {
-                handledInput.Add(temp);
+                handledInput.Add(temp.Singularize());
             }
         }
 
         return handledInput;
+    }
+
+    public static bool GetRexAnswer(string userQuestionTemp, out string result)
+    {
+        result = userQuestionTemp;
+
+        for (int i = 0; i < _pattern.Count; i++)
+        {
+            if (_pattern[i].IsMatch(userQuestionTemp.Trim()))
+            {
+                result = $"\"{userQuestionTemp}\"";
+                return true;
+            }
+        }
+        return false;
     }
 }
